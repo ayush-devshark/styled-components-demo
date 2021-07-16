7b8a96556089168809bc2ef69fcba0716d4b33f2
Ayush <a>
2021-07-16 18:27:56 +0530

    add themes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import Card from './components/Card';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './global-styles';
+import { DefaultButton } from './components/Buttons';
+
+const BaseTheme = {
+    color: '#222',
+    background: '#fff',
+};
+const DarkTheme = {
+    color: '#fff',
+    background: '#222',
+};
+
+const Container = styled.div`
+    padding: 2rem;
+    color: ${props => props.theme.color};
+    background-color: ${props => props.theme.background};
+`;
 
 function App() {
+    const [theme, setTheme] = useState(true);
+
+    const toggleTheme = () => {
+        setTheme(!theme);
+    };
+
     return (
-        <div style={{ padding: '2rem' }}>
+        <ThemeProvider theme={theme ? BaseTheme : DarkTheme}>
             <GlobalStyle />
-            <Card></Card>
-        </div>
+            <Container>
+                <h1>hello world</h1>
+                <DefaultButton onClick={toggleTheme}>toggle me</DefaultButton>
+            </Container>
+        </ThemeProvider>
     );
 }
 
