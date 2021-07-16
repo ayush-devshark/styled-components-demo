1d0e2eee9be53a91ef83dd8e91ac330496a34767
Ayush <a>
2021-07-16 18:47:29 +0530

    add As prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 import Loading from './components/Loading';
+import { DefaultButton } from './components/Buttons';
 
 function App() {
     return (
         <div style={{ padding: '2rem' }}>
-            <Loading />
+            <DefaultButton>Click Me!</DefaultButton>
+            <DefaultButton
+                as='a'
+                href='https://www.a11yproject.com/#explore-resources'
+            >
+                Click Me!
+            </DefaultButton>
+            <DefaultButton>Click Me!</DefaultButton>
         </div>
     );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,10 +7,15 @@ export const DefaultButton = styled.button`
     border-radius: 0.25rem;
     cursor: pointer;
     text-transform: capitalize;
-    padding: 0.5rem;
+    padding: 1rem;
     display: block;
-    width: 200px;
-    margin: 1rem auto;
+    width: 150px;
+    margin-right: 1rem;
+    display: inline-block;
+
+    text-decoration: none;
+    text-align: center;
+    font-size: 1rem;
 `;
 
 export const WhiteButton = styled(DefaultButton)`
