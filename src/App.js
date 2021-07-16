a661e3dd5c5733baff76e0e2d43e9438d52f1f17
Ayush <a>
2021-07-16 18:39:42 +0530

    add animations

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import React, { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import GlobalStyle from './global-styles';
-import { DefaultButton } from './components/Buttons';
-
-const BaseTheme = {
-    color: '#222',
-    background: '#fff',
-};
-const DarkTheme = {
-    color: '#fff',
-    background: '#222',
-};
-
-const Container = styled.div`
-    padding: 2rem;
-    color: ${props => props.theme.color};
-    background-color: ${props => props.theme.background};
-`;
+import styled from 'styled-components';
+import Loading from './components/Loading';
 
 function App() {
-    const [theme, setTheme] = useState(true);
-
-    const toggleTheme = () => {
-        setTheme(!theme);
-    };
-
     return (
-        <ThemeProvider theme={theme ? BaseTheme : DarkTheme}>
-            <GlobalStyle />
-            <Container>
-                <h1>hello world</h1>
-                <DefaultButton onClick={toggleTheme}>toggle me</DefaultButton>
-            </Container>
-        </ThemeProvider>
+        <div style={{ padding: '2rem' }}>
+            <Loading />
+        </div>
     );
 }
 
